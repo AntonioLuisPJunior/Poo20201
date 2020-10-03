ed1c6b69db0bc38d1ca83d3986cc1637238b03dc
RicardoGC <ricardogc379@gmail.com>
2020-10-02 22:39:52 -0300

    Hello World

---
+++ HelloWord.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world! Git\n\n");
+    System.out.println("Hello world! Teste\n\n");
+    System.out.println("Hello world! Teste\n\n");
+    System.out.println("Hello world! RICARDO");
+  }
+}
