b76e04fe8479fcc5e68673b8f7469c9d8438570d
Laryssa <laryssasousa@aluno.uema.br>
2020-11-22 20:33:47 -0300

    aula

---
+++ Aula2/Olamundo.java
@@ -0,0 +1,11 @@
+public class Olamundo {
+    public static void main(String... args) {
+      //comentario
+      System.out.println(  "ola mundo"   );
+      System.out.println(   "ola mundo"   );
+      /*muitos
+      comentarios*/
+      System.out.println("ola mundo   ");
+  
+    }
+  }
